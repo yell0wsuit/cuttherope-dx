c5099a5e211bf11109afbf1a3390fae61955732d
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2025-11-11 21:06:07 +0700

    decouple object loading methods

---
+++ CutTheRope/game/loadObjects/LoadPumps.cs
@@ -0,0 +1,34 @@
+using CutTheRope.desktop;
+using CutTheRope.iframework;
+using CutTheRope.iframework.core;
+using CutTheRope.iframework.helpers;
+using CutTheRope.iframework.sfe;
+using CutTheRope.iframework.visual;
+using CutTheRope.ios;
+
+namespace CutTheRope.game
+{
+    /// <summary>
+    /// Handles loading pump objects from XML level data
+    /// </summary>
+    internal sealed partial class GameScene
+    {
+        private void LoadPump(XMLNode item, float scale, float offsetX, float offsetY, int mapOffsetX, int mapOffsetY)
+        {
+            if (item.Name != "pump") return;
+
+            Pump pump = Pump.Pump_createWithResID(83);
+            pump.DoRestoreCutTransparency();
+            _ = pump.AddAnimationWithDelayLoopedCountSequence(0.05f, Timeline.LoopType.TIMELINE_NO_LOOP, 4, 1, [2, 3, 0]);
+            pump.bb = MakeRectangle(300f, 300f, 175f, 175f);
+            pump.initial_x = pump.x = (item["x"].IntValue() * scale) + offsetX + mapOffsetX;
+            pump.initial_y = pump.y = (item["y"].IntValue() * scale) + offsetY + mapOffsetY;
+            pump.initial_rotation = 0f;
+            pump.initial_rotatedCircle = null;
+            pump.rotation = item["angle"].FloatValue() + 90f;
+            pump.UpdateRotation();
+            pump.anchor = 18;
+            _ = pumps.AddObject(pump);
+        }
+    }
+}
