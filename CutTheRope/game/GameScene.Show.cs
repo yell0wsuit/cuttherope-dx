095f1050bdb7879047cd17fe3d052fb4557b169b
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2025-11-13 22:16:28 +0700

    decouple more objects from `GameScene.Show()`

--- CutTheRope/game/GameScene.Show.cs
+++ CutTheRope/game/GameScene.Show.cs
@@ -36,13 +36,7 @@ namespace CutTheRope.game
                 {
                     if (item3.Name == "gravitySwitch")
                     {
-                        gravityButton = CreateGravityButtonWithDelegate(this);
-                        gravityButton.visible = false;
-                        gravityButton.touchable = false;
-                        _ = AddChild(gravityButton);
-                        gravityButton.x = (item3["x"].IntValue() * num) + num3 + num4;
-                        gravityButton.y = (item3["y"].IntValue() * num) + num2 + num5;
-                        gravityButton.anchor = 18;
+                        LoadGravityButton(item3, num, num3 + num4, num2 + num5, 0, 0);
                     }
                     else if (item3.Name == "star")
                     {
@@ -86,91 +80,7 @@ namespace CutTheRope.game
                     }
                     else if (item3.Name == "target")
                     {
-                        int pack = ((CTRRootController)Application.SharedRootController()).GetPack();
-                        support = Image.Image_createWithResIDQuad(100, pack);
-                        support.Retain();
-                        support.DoRestoreCutTransparency();
-                        support.anchor = 18;
-                        target = CharAnimations.CharAnimations_createWithResID(80);
-                        target.DoRestoreCutTransparency();
-                        target.passColorToChilds = false;
-                        NSString nSString3 = item3["x"];
-                        target.x = support.x = (nSString3.IntValue() * num) + num3 + num4;
-                        NSString nSString4 = item3["y"];
-                        target.y = support.y = (nSString4.IntValue() * num) + num2 + num5;
-                        target.AddImage(101);
-                        target.AddImage(102);
-                        target.bb = MakeRectangle(264.0, 350.0, 108.0, 2.0);
-                        target.AddAnimationWithIDDelayLoopFirstLast(0, 0.05f, Timeline.LoopType.TIMELINE_REPLAY, 0, 18);
-                        target.AddAnimationWithIDDelayLoopFirstLast(1, 0.05f, Timeline.LoopType.TIMELINE_NO_LOOP, 43, 67);
-                        int num14 = 68;
-                        target.AddAnimationWithIDDelayLoopCountSequence(2, 0.05f, Timeline.LoopType.TIMELINE_NO_LOOP, 32, num14,
-                        [
-                            num14 + 1,
-                            num14 + 2,
-                            num14 + 3,
-                            num14 + 4,
-                            num14 + 5,
-                            num14 + 6,
-                            num14 + 7,
-                            num14 + 8,
-                            num14 + 9,
-                            num14 + 10,
-                            num14 + 11,
-                            num14 + 12,
-                            num14 + 13,
-                            num14 + 14,
-                            num14 + 15,
-                            num14,
-                            num14 + 1,
-                            num14 + 2,
-                            num14 + 3,
-                            num14 + 4,
-                            num14 + 5,
-                            num14 + 6,
-                            num14 + 7,
-                            num14 + 8,
-                            num14 + 9,
-                            num14 + 10,
-                            num14 + 11,
-                            num14 + 12,
-                            num14 + 13,
-                            num14 + 14,
-                            num14 + 15
-                        ]);
-                        target.AddAnimationWithIDDelayLoopFirstLast(7, 0.05f, Timeline.LoopType.TIMELINE_NO_LOOP, 19, 27);
-                        target.AddAnimationWithIDDelayLoopFirstLast(8, 0.05f, Timeline.LoopType.TIMELINE_NO_LOOP, 28, 31);
-                        target.AddAnimationWithIDDelayLoopFirstLast(9, 0.05f, Timeline.LoopType.TIMELINE_REPLAY, 32, 40);
-                        target.AddAnimationWithIDDelayLoopFirstLast(6, 0.05f, Timeline.LoopType.TIMELINE_NO_LOOP, 28, 31);
-                        target.AddAnimationWithIDDelayLoopFirstLast(101, 10, 0.05f, Timeline.LoopType.TIMELINE_NO_LOOP, 47, 76);
-                        target.AddAnimationWithIDDelayLoopFirstLast(101, 3, 0.05f, Timeline.LoopType.TIMELINE_NO_LOOP, 0, 19);
-                        target.AddAnimationWithIDDelayLoopFirstLast(101, 4, 0.05f, Timeline.LoopType.TIMELINE_NO_LOOP, 20, 46);
-                        target.AddAnimationWithIDDelayLoopFirstLast(102, 5, 0.05f, Timeline.LoopType.TIMELINE_NO_LOOP, 0, 12);
-                        target.SwitchToAnimationatEndOfAnimationDelay(9, 6, 0.05f);
-                        target.SwitchToAnimationatEndOfAnimationDelay(101, 4, 80, 8, 0.05f);
-                        target.SwitchToAnimationatEndOfAnimationDelay(80, 0, 101, 10, 0.05f);
-                        target.SwitchToAnimationatEndOfAnimationDelay(80, 0, 80, 1, 0.05f);
-                        target.SwitchToAnimationatEndOfAnimationDelay(80, 0, 80, 2, 0.05f);
-                        target.SwitchToAnimationatEndOfAnimationDelay(80, 0, 101, 3, 0.05f);
-                        target.SwitchToAnimationatEndOfAnimationDelay(80, 0, 101, 4, 0.05f);
-                        target.Retain();
-                        if (CTRRootController.IsShowGreeting())
-                        {
-                            dd.CallObjectSelectorParamafterDelay(new DelayedDispatcher.DispatchFunc(Selector_showGreeting), null, 1.3f);
-                            CTRRootController.SetShowGreeting(false);
-                        }
-                        target.PlayTimeline(0);
-                        target.GetTimeline(0).delegateTimelineDelegate = this;
-                        target.SetPauseAtIndexforAnimation(8, 7);
-                        blink = Animation.Animation_createWithResID(80);
-                        blink.parentAnchor = 9;
-                        blink.visible = false;
-                        blink.AddAnimationWithIDDelayLoopCountSequence(0, 0.05f, Timeline.LoopType.TIMELINE_NO_LOOP, 4, 41, [41, 42, 42, 42]);
-                        blink.SetActionTargetParamSubParamAtIndexforAnimation("ACTION_SET_VISIBLE", blink, 0, 0, 2, 0);
-                        blinkTimer = 3;
-                        blink.DoRestoreCutTransparency();
-                        _ = target.AddChild(blink);
-                        idlesTimer = RND_RANGE(5, 20);
+                        LoadTarget(item3, num, num3 + num4, num2 + num5, 0, 0);
                     }
                 }
             }
---
+++ CutTheRope/game/LoadObjects/LoadGravityButton.cs
@@ -0,0 +1,26 @@
+using CutTheRope.ios;
+
+namespace CutTheRope.game
+{
+    /// <summary>
+    /// Handles loading the gravity switch button from XML level data
+    /// The gravity button allows the player to toggle gravity direction
+    /// </summary>
+    internal sealed partial class GameScene
+    {
+        /// <summary>
+        /// Loads the gravity switch button from XML node data
+        /// Creates and positions the gravity toggle button
+        /// </summary>
+        private void LoadGravityButton(XMLNode xmlNode, float scale, float offsetX, float offsetY, int mapOffsetX, int mapOffsetY)
+        {
+            gravityButton = CreateGravityButtonWithDelegate(this);
+            gravityButton.visible = false;
+            gravityButton.touchable = false;
+            _ = AddChild(gravityButton);
+            gravityButton.x = (xmlNode["x"].IntValue() * scale) + offsetX + mapOffsetX;
+            gravityButton.y = (xmlNode["y"].IntValue() * scale) + offsetY + mapOffsetY;
+            gravityButton.anchor = 18;
+        }
+    }
+}
