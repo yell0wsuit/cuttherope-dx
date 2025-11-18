e2558244ab9248680a2a0ac4efee5156fa28ff9c
Bingies24 <74263388+Bingies24@users.noreply.github.com>
2025-11-17 21:41:46 -0500

    Revert "New project setup for cross platform"
    This reverts commit 8fe50851644143319c95ad7a189222279454bfe3.

--- build_linux.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd CutTheRope.DesktopGL
-dotnet publish -p:PublishProfile=Windows64 --runtime win-x64
-dotnet publish -p:PublishProfile=Linux64 --runtime linux-x64
--- build_windows.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd CutTheRope.DesktopGL
-dotnet publish -p:PublishProfile=Windows64 --runtime win-x64
-dotnet publish -p:PublishProfile=Linux64 --runtime linux-x64
