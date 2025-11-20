e0b77c9e4f4f0c54ec5fdeebd8ca179c76fcd292
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2025-11-20 15:01:49 +0700

    fix cloud vfx for bubble form when candy is in it

--- CutTheRope/GameMain/CandyInGhostBubbleAnimation.cs
+++ CutTheRope/GameMain/CandyInGhostBubbleAnimation.cs
@@ -25,7 +25,7 @@ namespace CutTheRope.GameMain
         public void AddSupportingCloudsTimelines()
         {
             backCloud = Image_createWithResIDQuad(180, 6);
-            backCloud.x = x + 75f;
+            backCloud.x = x + 85f;
             backCloud.y = y + 25f;
             backCloud.anchor = backCloud.parentAnchor = 18;
             _ = AddChild(backCloud);
@@ -45,7 +45,7 @@ namespace CutTheRope.GameMain
             backCloud.PlayTimeline(0);
 
             backCloud2 = Image_createWithResIDQuad(180, 5);
-            backCloud2.x = x + 55f;
+            backCloud2.x = x + 65f;
             backCloud2.y = y + 55f;
             backCloud2.anchor = backCloud2.parentAnchor = 18;
             _ = AddChild(backCloud2);
@@ -65,8 +65,8 @@ namespace CutTheRope.GameMain
             backCloud2.PlayTimeline(0);
 
             backCloud3 = Image_createWithResIDQuad(180, 5);
-            backCloud3.x = x - 75f;
-            backCloud3.y = y + 20f;
+            backCloud3.x = x - 90f;
+            backCloud3.y = y + 15f;
             backCloud3.anchor = backCloud3.parentAnchor = 18;
             _ = AddChild(backCloud3);
             Timeline timeline3 = new Timeline().InitWithMaxKeyFramesOnTrack(5);
@@ -85,8 +85,8 @@ namespace CutTheRope.GameMain
             backCloud3.PlayTimeline(0);
 
             Image image = Image_createWithResIDQuad(180, 6);
-            image.x = x - 65f;
-            image.y = y + 50f;
+            image.x = x - 75f;
+            image.y = y + 45f;
             image.anchor = image.parentAnchor = 18;
             //image.DoRestoreCutTransparency();
             _ = AddChild(image);
@@ -106,8 +106,8 @@ namespace CutTheRope.GameMain
             image.PlayTimeline(0);
 
             Image image2 = Image_createWithResIDQuad(180, 2);
-            image2.x = x - 10f;
-            image2.y = y + 65f;
+            image2.x = x - 20f;
+            image2.y = y + 75f;
             image2.anchor = image2.parentAnchor = 18;
             //image2.DoRestoreCutTransparency();
             _ = AddChild(image2);
